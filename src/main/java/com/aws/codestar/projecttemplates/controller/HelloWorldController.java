eb7f23eddf8903303af2343a5fa4e52a9ce37a37
Zach Marbach <zachary.marbach@improving.com>
2020-04-11 22:58:52 -0500

    add base url to app.properties

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,7 +11,6 @@ import org.springframework.web.servlet.ModelAndView;
  * Basic Spring MVC controller that handles all GET requests.
  */
 @Controller
-@RequestMapping("/")
 public class HelloWorldController {
 
     private final String siteName;
@@ -18,9 +19,16 @@ public class HelloWorldController {
         this.siteName = siteName;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView helloWorld() {
-        ModelAndView mav = new ModelAndView("index");
+//    @GetMapping("/")
+//    public ModelAndView helloWorld() {
+//        ModelAndView mav = new ModelAndView("index");
+//        mav.addObject("siteName", this.siteName);
+//        return mav;
+//    }
+
+    @GetMapping(path = "${base.url}")
+    public ModelAndView test() {
+        ModelAndView mav = new ModelAndView("index-test");
         mav.addObject("siteName", this.siteName);
         return mav;
     }
