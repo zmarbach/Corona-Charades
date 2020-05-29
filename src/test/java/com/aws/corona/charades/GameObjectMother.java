a8d4c55596a4d0e931f6a3be64d8c0a4bab25853
Zach Marbach <zachary.marbach@improving.com>
2020-05-29 13:21:08 -0500

    fix object mother classes

--- src/test/java/com/aws/corona/charades/GameObjectMother.java
+++ src/test/java/com/aws/corona/charades/GameObjectMother.java
@@ -13,12 +13,10 @@ import java.util.UUID;
 @Component
 public class GameObjectMother {
 
-    private TeamObjectMother teamObjectMother = new TeamObjectMother();
-    private PlayerObjectMother playerObjectMother = new PlayerObjectMother();
+    private GameObjectMother(){
+    }
 
-    public Game buildGame(UUID uuid, Team teamOne, Team teamTwo,
-                          List<String> activeWords, List<String> guessedWords,
-                          String currentWord, Player currentPlayer, boolean newTurn){
+    public static Game buildGame(UUID uuid, Team teamOne, Team teamTwo, List<String> activeWords, List<String> guessedWords, String currentWord, Player currentPlayer, boolean newTurn){
         return new Game.GameBuilder()
                 .withUUID(uuid)
                 .withTeamOne(teamOne)
@@ -31,28 +29,26 @@ public class GameObjectMother {
                 .build();
     }
 
-    public Game setUpGame(){
-        Team team1 = teamObjectMother.createTeam("Team One", 0, new ArrayList<>(), new Player());
-        Player team1Player1 = playerObjectMother.createPlayer("Andrew", team1);
-        Player team1Player2 = playerObjectMother.createPlayer("Billy", team1);
+    public static Game setUpGame(){
+        Team team1 = TeamObjectMother.createTeam("Team One", 0, new ArrayList<>(), new Player());
+        Player team1Player1 = PlayerObjectMother.createPlayer("Andrew", team1);
+        Player team1Player2 = PlayerObjectMother.createPlayer("Billy", team1);
         setPlayersAndPrevPlayer(team1, team1Player1, Arrays.asList(team1Player1, team1Player2));
 
-        Team team2 = teamObjectMother.createTeam("Team Two", 0, new ArrayList<>(), new Player());
-        Player team2Player1 = playerObjectMother.createPlayer("Chad", team2);
-        Player team2Player2 = playerObjectMother.createPlayer("Derek", team2);
+        Team team2 = TeamObjectMother.createTeam("Team Two", 0, new ArrayList<>(), new Player());
+        Player team2Player1 = PlayerObjectMother.createPlayer("Chad", team2);
+        Player team2Player2 = PlayerObjectMother.createPlayer("Derek", team2);
         setPlayersAndPrevPlayer(team2, team2Player1, Arrays.asList(team2Player1, team2Player2));
 
         List<String> activeWords = createActiveWords();
         return buildGame(UUID.randomUUID(), team1, team2, activeWords, new ArrayList<>(), "mockCurrentWord", team1.getPlayers().get(0), true );
     }
 
-    private List<String> createActiveWords() {
-        List<String> activeWords = new ArrayList<>();
-        activeWords.addAll(Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliette"));
-        return activeWords;
+    private static List<String> createActiveWords() {
+        return new ArrayList<>(Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliette"));
     }
 
-    private void setPlayersAndPrevPlayer(Team team, Player prevPlayer, List<Player> players) {
+    private static void setPlayersAndPrevPlayer(Team team, Player prevPlayer, List<Player> players) {
         for (Player player : players) {
             team.getPlayers().add(player);
         }
